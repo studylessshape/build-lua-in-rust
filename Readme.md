ae304f011a2976013e4139d1048271e566902fd1
studylessshape <a664554724@qq.com>
2024-11-12 10:46:33 +0800

    to: ch01-02.byte_codes

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# Build lua in rust
+From [build lua in rust](https://wubingzheng.github.io/build-lua-in-rust/zh/)
---
+++ crates/lua-rs/src/lib.rs
@@ -0,0 +1,10 @@
+#[derive(Debug)]
+pub enum ByteCode{
+    GetGlobal(u8, u8),
+    LocalConst(u8, u8),
+    Call(u8, u8)
+}
+
+#[cfg(test)]
+mod tests {
+}
