c2417cdd63ae6dad9200178e44b1ce20e85350ea
studylessshape <a664554724@qq.com>
2024-11-12 10:56:25 +0800

    to: ch01-03.value_and_type

--- crates/lua-rs/src/lib.rs
+++ crates/lua-rs/src/lib.rs
@@ -5,6 +5,23 @@ pub enum ByteCode{
     Call(u8, u8)
 }
 
+#[derive(Clone, Debug)]
+pub enum Value {
+    Nil,
+    String(String),
+    Function(fn (&mut i32) -> i32),
+}
+
+// impl fmt::Debug for Value {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         match self {
+//             Self::Nil => write!(f, "Nil"),
+//             Self::String(arg0) => f.debug_tuple("String").field(arg0).finish(),
+//             Self::Function(arg0) => f.debug_tuple("Function").field(arg0).finish(),
+//         }
+//     }
+// }
+
 #[cfg(test)]
 mod tests {
 }
